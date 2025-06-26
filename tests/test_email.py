b8a544f6ea19e18eea0ea4aeff65a585f86bba35
luzhac <luzhac@gmail.com>
2025-06-26 17:52:44 +0100

    a

--- tests/test_email.py
+++ tests/test_email.py
@@ -1,9 +1,9 @@
 from unittest.mock import patch, MagicMock
 from src.utils import smtp_send_mail
 
+@patch('src.utils.smtplib.SMTP_SSL')
 @patch('src.utils.os.getenv')
-@patch('src.utils.smtplib.SMTP')
-def test_smtp_send_mail_success(mock_smtp, mock_getenv):
+def test_smtp_send_mail_ssl(mock_getenv, mock_smtp_ssl):
     mock_getenv.side_effect = lambda key: {
         'gmail_user': 'test@example.com',
         'gmail_password': 'password',
@@ -11,7 +11,7 @@ def test_smtp_send_mail_success(mock_smtp, mock_getenv):
     }.get(key)
 
     mock_server = MagicMock()
-    mock_smtp.return_value = mock_server
+    mock_smtp_ssl.return_value.__enter__.return_value = mock_server
 
     result = smtp_send_mail("Test Subject", "Test Body")
 
@@ -19,9 +19,9 @@ def test_smtp_send_mail_success(mock_smtp, mock_getenv):
 
 
 
+@patch('src.utils.smtplib.SMTP_SSL')
 @patch('src.utils.os.getenv')
-@patch('src.utils.smtplib.SMTP')
-def test_duplicate_email_not_sent(mock_smtp, mock_getenv):
+def test_duplicate_email_not_sent(mock_getenv, mock_smtp_ssl):
     # test duplicate email
     mock_getenv.side_effect = lambda key: {
         'gmail_user': 'test@example.com',
@@ -30,7 +30,7 @@ def test_duplicate_email_not_sent(mock_smtp, mock_getenv):
     }.get(key)
 
     mock_server = MagicMock()
-    mock_smtp.return_value = mock_server
+    mock_smtp_ssl.return_value.__enter__.return_value = mock_server
 
     subject = "Duplicate"
     body = "Same body"
